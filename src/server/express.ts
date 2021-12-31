0189be7ac9c18004bf8b86156854ae0b27ddc00b
ibrahim <ib.arshad777@gmail.com>
2021-12-31 20:13:16 +0600

    Create express server

---
+++ src/server/express.ts
@@ -0,0 +1,51 @@
+import express from "express";
+import type { Request, Response, NextFunction } from "express";
+
+function handleErrors(fn: (req: Request, res: Response) => Promise<void>) {
+  return async function (req: Request, res: Response, next: NextFunction) {
+    try {
+      return await fn(req, res);
+    } catch (x) {
+      next(x);
+    }
+  };
+}
+
+const isPipe = (portOrPipe: number | string) => Number.isNaN(portOrPipe);
+
+const PORT = 4500;
+const app = express();
+
+app.use(express.static("build"));
+// app.use(express.static('public'));
+
+app.get(
+  "/",
+  handleErrors(async function (req, res) {
+    // render(req.url, res);
+  })
+);
+
+app
+  .listen(PORT, () => {
+    console.log(`Listening at ${PORT}...`);
+  })
+  .on("error", function (error: NodeJS.ErrnoException) {
+    if (error.syscall !== "listen") {
+      throw error;
+    }
+
+    const bind = isPipe(PORT) ? "Pipe " + PORT : "Port " + PORT;
+    switch (error.code) {
+      case "EACCES":
+        console.error(bind + " requires elevated privileges");
+        process.exit(1);
+        break;
+      case "EADDRINUSE":
+        console.error(bind + " is already in use");
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  });
